50cc7909769085cd116f445d78f26823ccc65487
Esteban <91676362+estebanbaigts@users.noreply.github.com>
2024-11-13 13:40:41 +0100

    [ADD] tout les dependances

--- elixir/tailwind.config.js
+++ elixir/tailwind.config.js
@@ -1,10 +1,25 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#8B5CF6',
+          dark: '#6D28D9',
+        },
+        dark: '#1A1A1A',
+        white: {
+          DEFAULT: '#FFFFFF'
+        }
+      },
+      animation: {
+        'bounce-slow': 'bounce 3s infinite',
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      }
+    },
   },
   plugins: [],
-}
+};
